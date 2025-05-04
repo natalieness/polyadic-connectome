5e8a04f1816d864159b3d9268e1eafa3c92637cf
natalieness <natalie.ness@outlook.com>
2025-05-04 17:36:02 +0100

    separated little helper and graph functions

--- scripts/little_helper.py
+++ scripts/little_helper.py
@@ -1,5 +1,7 @@
-import pandas as pd
-import numpy as np
+'''Little helper functions '''
+
+
+### just printing a bunch of info about the data ###
 
 def inspect_data(links_df, verbose=True):
     n_entries = links_df.shape[0]
@@ -16,3 +18,29 @@ def inspect_data(links_df, verbose=True):
         print(f"Number of postsynaptic sites: {n_postsynaptic}")
         print(f"Number of presynaptic sites: {n_presynaptic}")
     return n_entries, n_connectors, n_skeletons, n_nodes, n_postsynaptic, n_presynaptic
+
+### Mapping skids to cell types ###
+
+def get_celltype_dict(celltype_df):
+    skid_to_celltype = {
+        skid: row['name']
+        for _, row in celltype_df.iterrows()
+        for skid in row['skids']
+    }
+    return skid_to_celltype
+
+def get_celltype_name(skid, skid_to_celltype=skid_to_celltype):
+    return skid_to_celltype.get(skid, "NA")  # Returns "NA" if skid is not found
+
+### Mapping skids to cell types in specific structures ###
+
+def celltype_col_for_list(connector_df, col_name, new_col_name='postsynaptic_celltype'):
+    df_series = connector_df[col_name]
+    new_df_series = []
+    for l in df_series:
+        #each element is a list of skids 
+        new_l = []
+        for skid in l:
+            new_l.append(get_celltype_name(skid))
+        new_df_series.append(new_l)
+    connector_df[new_col_name] = new_df_series
--- scripts/map_synskids.py
+++ scripts/map_synskids.py
@@ -4,20 +4,33 @@ postsynaptic sites. Using hand annotated neuronal celltypes from catmaid
 #%%
 from itertools import chain
 from collections import Counter
+from collections import defaultdict
+from itertools import combinations
+import os
+
+import networkx as nx
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
+
 from contools import Celltype, Celltype_Analyzer, Promat
 import pymaid
 from pymaid_creds import url, name, password, token
-from scripts.little_helper import inspect_data
-from collections import defaultdict
-from itertools import combinations
-import networkx as nx
+
+# local imports
+from scripts.little_helper import inspect_data, get_celltype_dict, get_celltype_name, celltype_col_for_list
+from scripts.undirected_graph_functions import construct_polyadic_incidence_matrix, construct_group_projection_matrix
+from scripts.undirected_graph_functions import get_group_pair_counts, build_group_graph, graph_normalize_weights, plot_nx_graph, centered_subgraph
+
 
 rm = pymaid.CatmaidInstance(url, token, name, password)
+
+#get parent directory path
+current_file = __file__  
+current_dir = os.path.dirname(current_file)
+parent_dir = os.path.dirname(current_dir)
+path_for_data = parent_dir+'/data/'
 
 #%% get and describe neuronal identity data 
 celltype_df,celltypes = Celltype_Analyzer.default_celltypes()
@@ -78,15 +91,8 @@ n_labelled = has_labelled_neuron.sum()
 links_with_labelled = links_with_presyn[links_with_presyn['connector_id'].isin(has_labelled_neuron[has_labelled_neuron].index)]
 print(f"Number of connectors with presynapse that belong to labelled neurons: {n_labelled}")
 links_with_labelled = links_with_labelled.reset_index(drop=True)
-# %% easily map skids to celltypes
-
-skid_to_celltype = {
-    skid: row['name']
-    for _, row in celltype_df.iterrows()
-    for skid in row['skids']
-}
-def get_celltype_name(skid, skid_to_celltype=skid_to_celltype):
-    return skid_to_celltype.get(skid, "NA")  # Returns "NA" if skid is not found
+# %% get dictionary to map skids to celltypes 
+skid_to_celltype = get_celltype_dict(celltype_df)
 
 
 # %% to do set this up so you can use connector_details to easily check identity at each intersection
@@ -94,22 +100,9 @@ def get_celltype_name(skid, skid_to_celltype=skid_to_celltype):
 # remove connector details without presynaptic site 
 connector_details = connector_details.dropna(subset=['presynaptic_to'])
 
-
-
 # %% # map skid ids in connector details to celltypes
 
 connector_details['presynaptic_celltype'] = connector_details['presynaptic_to'].apply(get_celltype_name)
-
-def celltype_col_for_list(connector_df, col_name, new_col_name='postsynaptic_celltype'):
-    df_series = connector_df[col_name]
-    new_df_series = []
-    for l in df_series:
-        #each element is a list of skids 
-        new_l = []
-        for skid in l:
-            new_l.append(get_celltype_name(skid))
-        new_df_series.append(new_l)
-    connector_df[new_col_name] = new_df_series
 
 celltype_col_for_list(connector_details, 'postsynaptic_to', new_col_name='postsynaptic_celltype')
 
@@ -347,47 +340,7 @@ ct_P_alone = get_P_alone_all(labelled_connectors['postsynaptic_celltype'], cellt
 
 # construct a multilevel hypergraph to represent the polyadic data structure
 
-def construct_polayadic_incidence_matrix(list_of_connectors):
-    ''' Construct a polyadic incidence matrix from a list of connectors '''
-    # force list 
-    if not isinstance(list_of_connectors, list):
-        list_of_connectors = list(list_of_connectors)
-    # get all unique skids
-    all_skids = tuple(set(chain.from_iterable(list_of_connectors)))
-    n_skids = len(all_skids)
-    n_connectors = len(list_of_connectors)
-
-    # create incidence matrix 
-    IM = np.zeros((n_skids, n_connectors))
-    for e, connector in enumerate(list_of_connectors):
-        for skid in connector:
-            skid_idx = list(all_skids).index(skid)
-            IM[skid_idx, e] += 1
-    return IM, all_skids
-
-def construct_group_projection_matrix(IM, all_skids, skid_to_celltype):
-
-    # get all unique cell types from dictionary 
-    all_celltypes = tuple(set(skid_to_celltype.values()))
-    n_celltypes = len(all_celltypes)
-
-    n_edges = IM.shape[1]
-
-    # create group projection matrix
-    GPM = np.zeros((n_celltypes, n_edges))
-    for e in range(n_edges):
-        # get all skids in connector
-        connector_skids = np.where(IM[:, e] > 0)[0]
-        # get all celltypes in connector
-        connector_celltypes = [skid_to_celltype[all_skids[skid]] for skid in connector_skids]
-        for c in connector_celltypes:
-            ct_idx = list(all_celltypes).index(c)
-            GPM[ct_idx, e] += 1
-    return GPM, all_celltypes
-
-
-
-IM, all_skids = construct_polayadic_incidence_matrix(labelled_connectors['postsynaptic_to'])
+IM, all_skids = construct_polyadic_incidence_matrix(labelled_connectors['postsynaptic_to'])
 GPM, all_celltypes = construct_group_projection_matrix(IM, all_skids, skid_to_celltype)
 
 
@@ -432,139 +385,29 @@ hyperedges = list(labelled_connectors['postsynaptic_to'])
 # need a way to map vertex ids to group ids 
 # this is skid_to_celltype 
 
-
-# Track group pair co-occurrence counts
-group_pair_counts = defaultdict(int)
-
-def get_group_pair_counts(hyperedges, vertex_to_group=skid_to_celltype):
-    for hedge in hyperedges:
-        groups_in_edge = [vertex_to_group[v] for v in hedge]
-        unique_groups = set(groups_in_edge)
-
-        # Count all unordered group pairs (with self-pairs)
-        for g1, g2 in combinations(sorted(unique_groups), 2):
-            group_pair_counts[(g1, g2)] += 1
-
-        # Optionally include self-pairs (e.g., A–A if multiple A members)
-        for g in unique_groups:
-            if groups_in_edge.count(g) > 1:
-                group_pair_counts[(g, g)] += 1
-    return group_pair_counts
-
-def build_group_graph(group_pair_counts, vertex_to_group=skid_to_celltype):
-    G = nx.Graph()
-    # Add nodes for each group
-    all_groups = set(vertex_to_group.values())
-    G.add_nodes_from(all_groups)
-
-    for (g1, g2), count in group_pair_counts.items():
-        G.add_edge(g1, g2, weight=count)
-    return G
-
 group_pair_counts = get_group_pair_counts(hyperedges, vertex_to_group=skid_to_celltype)
 G = build_group_graph(group_pair_counts, vertex_to_group=skid_to_celltype)
 
 ''' TODO: probably normalise weights based on group size or number of edges
 '''
-def normalize_weights(G, factor='mean'):
-    ''' 
-    Normalize weights of edges in graph G based on: 
-    - 'mean': Mean weight of all edges
-    - 'log': Logarithm of the mean weight
-    - 'jaccard': Jaccard similarity of the group/ group overlap
-    '''
-    if factor == 'mean':
-        mean_weight = np.mean([G[u][v]['weight'] for u, v in G.edges()])
-        for u, v in G.edges():
-            G[u][v]['weight'] /= mean_weight
-    elif factor == 'log':
-        mean_weight = np.mean([G[u][v]['weight'] for u, v in G.edges()])
-        for u, v in G.edges():
-            G[u][v]['weight'] = np.log10(G[u][v]['weight'] / mean_weight)
-    elif factor == 'jaccard':
-        for u, v in G.edges():
-            w = G[u][v]['weight']
-            deg_u = sum(d['weight'] for _, _, d in G.edges(u, data=True))
-            deg_v = sum(d['weight'] for _, _, d in G.edges(v, data=True))
-            G[u][v]['weight'] = w / (deg_u + deg_v - w)
-    else: 
-        print("Unknown normalization factor. No normalization applied.")
-    return G
-G = normalize_weights(G, factor='mean')
 
+G = graph_normalize_weights(G, factor='mean')
 
 
 # %% plot group graph
 
-def plot_nx_graph(G, plot_scale=1):
-
-    #pos = nx.circular_layout(G.subgraph(G.nodes))
-    pos = nx.nx_agraph.graphviz_layout(G, prog='twopi')
-    edge_weights = [G[u][v]['weight'] for u, v in G.edges()]
-
-    #scale all weights by a factor for visualization
-    edge_weights= [i*plot_scale for i in edge_weights]
-    nx.draw(
-        G, pos,
-        with_labels=True,
-        width=edge_weights,  # Line thickness ~ frequency
-        node_color='lightblue',
-        node_size=2000,
-        font_size=10,
-        edge_color='black' 
-    )
-    plt.title("Projected Group Interaction Graph")
-
-plot_nx_graph(G, plot_scale=1)
+plot_nx_graph(G, plot_scale=1, save_fig=True, path=path_for_data+'group_graph.png')
 
 # %% plot group graph from perspective of one group 
-
-def centered_subgraph(G, center_node, norm='group_participation', plot_scale=20):
-    '''
-    Normalization options for weights:
-    - 'group participation: Normalizes based on total participation of central group
-    '''
-    # Creat new graph of 
-    H = nx.Graph() 
-    
-    # Add all original nodes
-    H.add_nodes_from(G.nodes(data=True))
-
-    # Add only edges connected to the specified node
-    for neighbor in G.neighbors(center_node):
-        edge_data = G.get_edge_data(center_node, neighbor)
-        H.add_edge(center_node, neighbor, **edge_data)
-    
-    #plot subgraph
-    edge_nodes = H.nodes - center_node
-    pos = nx.circular_layout(H.subgraph(edge_nodes))
-    pos[center_node] = (0, 0)  # Center node at origin
-    edge_weights = [H[u][v]['weight'] for u, v in H.edges()]
-    # Normalize edge weights based on the specified normalization method
-    if norm == 'group_participation':
-        # Normalize by the total participation of the center node
-        total_participation = sum(edge_weights)
-        edge_weights = [w / total_participation for w in edge_weights]
-    # thicken edges based on given scale for visualization
-    edge_weights= [i*plot_scale for i in edge_weights]
-    nx.draw(
-        H, pos,
-        with_labels=True,
-        width=edge_weights,  # Line thickness ~ frequency
-        node_color='lightblue',
-        node_size=2000,
-        font_size=10,
-        edge_color='black' 
-    )
-    plt.title(f"Subgraph centered on {center_node}")
-    return H
 
 FFNs_subgraph = centered_subgraph(G, 'FFNs', norm='group_participation')
 
 # %%
-
+path_for_subgraphs = path_for_data + 'group_graphs/'
+if not os.path.exists(path_for_subgraphs):
+    os.makedirs(path_for_subgraphs)
 for ct in celltype_df['name'].unique():
     plt.figure()
-    centered_subgraph(G, ct, norm='group_participation', plot_scale=20)
+    centered_subgraph(G, ct, norm='group_participation', plot_scale=20, save_fig=True, path=path_for_subgraphs+f'{ct}_subgraph.png')
 
 # %%
