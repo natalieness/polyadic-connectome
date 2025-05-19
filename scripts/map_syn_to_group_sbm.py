e68a684a515cb4c40f594c5fb98f65e7b650c2b9
natalieness <natalie.ness@outlook.com>
2025-05-19 14:42:21 +0100

    added sbm by presynapse

--- scripts/map_syn_to_group_sbm.py
+++ scripts/map_syn_to_group_sbm.py
@@ -126,24 +126,21 @@ def get_postsynaptic_co_adj(hyperedges):
     ordered_postsynaptic = [post for post, _ in sorted(post_to_index.items(), key=lambda item: item[1])]
 
     return adj_matrix, ordered_postsynaptic
-
+#%% get SBM block probabilities for all labelled neurons
 
 hyperedges = labelled_connectors['postsynaptic_to'].tolist()
 ps_co_adj, ordered_ps_in_adj = get_postsynaptic_co_adj(hyperedges)
-
-# %% binarize the adj matrix in preparation for SBM model 
-
-# binarize the adjacency matrix
+# binarize the adj matrix in preparation for SBM model 
 ps_co_adj_bi = binarize(ps_co_adj)
 
 cmap = mpl.colors.ListedColormap(['white', 'black'])
 plt.imshow(ps_co_adj_bi, cmap=cmap)
 plt.axis('off')
 
-# %% get cell types for each postsynaptic partner in the adjacency matrix
+#  get cell types for each postsynaptic partner in the adjacency matrix
 ps_celltype_in_adj = [skid_to_celltype[post] for post in ordered_ps_in_adj]
 
-# %% get sbm group-to-group connection probabilities 
+#  get sbm group-to-group connection probabilities 
 
 
 estimator = SBMEstimator(directed=False, loops=True)
@@ -153,5 +150,81 @@ block_probs = pd.DataFrame(estimator.block_p_, index=np.unique(ps_celltype_in_ad
 sns.heatmap(block_probs, annot=False, fmt=".1f",cmap='Blues') 
 plt.title('Block probabilities for postsynaptic \nco-occurrence in polyadic synapses', fontsize=14)
 
+# %% 
+def get_sbm_block_probs_from_hyperedges(hyperedges, name='', plot=True):
+    """
+    Get the block probabilities for a given set of hyperedges.
+    """
+    # get the adjacency matrix
+    adj_matrix, ordered_ps_in_adj = get_postsynaptic_co_adj(hyperedges)
+    
+    # binarize the adjacency matrix
+    adj_matrix_bi = binarize(adj_matrix)
+    if plot:
+        cmap = mpl.colors.ListedColormap(['white', 'black'])
+        plt.imshow(adj_matrix_bi, cmap=cmap)
+        plt.axis('off')
+        plt.title(f'Adjacency matrix of polyadic partners\n({name})')
+        plt.show()
+    
+    # get the cell types for each postsynaptic partner
+    ps_celltype_in_adj = [skid_to_celltype[post] for post in ordered_ps_in_adj]
+    
+    # fit the SBM model
+    estimator = SBMEstimator(directed=False, loops=True)
+    estimator.fit(adj_matrix_bi, y=ps_celltype_in_adj)
+    block_probs = pd.DataFrame(estimator.block_p_, index=np.unique(ps_celltype_in_adj), columns=np.unique(ps_celltype_in_adj))
+    if plot:
+        sns.heatmap(block_probs, annot=False, fmt=".1f", cmap='Blues') 
+        plt.title(f'Block probabilities for postsynaptic co-occurrence \n in polyadic synapses ({name})', fontsize=14)
+        plt.show()
+    return adj_matrix_bi, block_probs, ps_celltype_in_adj
+    
+hyperedges = labelled_connectors['postsynaptic_to'].tolist()
+adj_all, block_probs_all, ps_celltype_in_adj_all = get_sbm_block_probs_from_hyperedges(hyperedges, name='all labelled neurons', plot=True)
+#%% get top block probabilities 
+
+def get_top_block_probs(block_probs, n=5, printing=True):
+    """
+    Get the top n block probabilities from a block probability matrix.
+    """
+    # get the upper triangle of the matrix
+    upper_triangle = np.triu(block_probs.values, k=0)
+    # get the indices of the top n block probabilities
+    top_indices = np.unravel_index(np.argsort(upper_triangle, axis=None)[-n:], upper_triangle.shape)
+    # get the top n block probabilities
+    top_block_probs = [(block_probs.index[i], block_probs.columns[j], upper_triangle[i, j]) for i, j in zip(*top_indices)]
+    if printing:
+        print("Top block probabilities:")
+        for i, (ct1, ct2, prob) in enumerate(top_block_probs[::-1]):
+            print(f"{i+1}. {ct1} - {ct2}: {prob:.2f}")
+
+    return top_block_probs
+top_block_probs = get_top_block_probs(block_probs_all, n=20)
+
+
 # %% Repeat but filtering by presynaptic cell type
 
+for ct in celltype_df['name'].unique():
+    print(f"Cell type: {ct}")
+    # get the hyperedges for the current cell type
+    hyperedges = labelled_connectors[labelled_connectors['presynaptic_celltype'] == ct]['postsynaptic_to'].tolist()
+    # get the block probabilities
+    adj_ct, block_probs_ct, ps_celltype_in_adj_ct = get_sbm_block_probs_from_hyperedges(hyperedges, name=f'Postsynaptic to {ct}', plot=True)
+    # get the top block probabilities
+    top_block_probs_ct = get_top_block_probs(block_probs_ct, n=10)
+    # save the block probabilities 
+    #block_probs.to_csv(path_for_data + f'block_probs_{ct}.csv')
+# %% statistically comparing sbm estimators for different subgraphs (based on presynaptic cell type)
+
+#example: kenyon cells and mushroom body output neurons
+kc_hyperedges = labelled_connectors[labelled_connectors['presynaptic_celltype'] == 'KCs']['postsynaptic_to'].tolist()
+adj_kc, block_probs_kc, kc_celltype_in_adj = get_sbm_block_probs_from_hyperedges(kc_hyperedges, name='KCs presynaptic', plot=True)
+
+mbon_hyperedges = labelled_connectors[labelled_connectors['presynaptic_celltype'] == 'MBONs']['postsynaptic_to'].tolist()
+adj_mbon, block_probs_mbon, mbon_celltype_in_adj = get_sbm_block_probs_from_hyperedges(mbon_hyperedges, name='MBONs presynaptic', plot=True)
+
+
+
+
+# %%
